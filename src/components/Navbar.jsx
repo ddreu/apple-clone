223212e224ba863b180ab346dc4b4912961fdfd1
Andrew <3922017@holycross.edu.ph>
2025-07-29 04:21:10 +0800

    highlights section and a video carousel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
           {navLists.map((nav) => (
             <div
               key={nav}
-              className="px-5 text-sm cursor-pointer text-gray-300 hover:text-white transition-all"
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
             >
               {nav}
             </div>
